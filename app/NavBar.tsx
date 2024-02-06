21a7b10cfd359ba199cf6e55cbbe45c299cb9ca3
Christine Yoo <christine.y@arketa.co>
2024-02-05 20:56:37 -0800

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 import { CgDebug } from "react-icons/cg";
+import classnames from "classnames";
 
 const NavBar = () => {
   const links = [
@@ -11,6 +14,8 @@ const NavBar = () => {
     { label: "Issues", href: "/issues" },
   ];
 
+  const pathname = usePathname();
+
   return (
     <nav className="flex px-5 h-14 border-b items-center space-x-6">
       <Link href="/">
@@ -18,7 +23,13 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <li className="text-pink-500 hover:text-pink-800 transition-colors">
+          <li
+            className={classnames({
+              "text-pink-900": link.href === pathname,
+              "text-pink-500": link.href !== pathname,
+              "hover:text-pink-800 transition-colors": true,
+            })}
+          >
             <Link href={link.href}>{link.label}</Link>
           </li>
         ))}
