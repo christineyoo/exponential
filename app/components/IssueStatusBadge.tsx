bdaa601712e4843a435841457ecc334696adfd28
Christine Yoo <christine.y@arketa.co>
2024-02-19 15:41:26 -0800

    Build the issue status badge

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,23 @@
+import { Status } from "@prisma/client";
+import { Badge } from "@radix-ui/themes";
+import React from "react";
+
+interface Props {
+  status: Status;
+}
+
+const statusMap: Record<
+  Status,
+  { label: string; color: "red" | "violet" | "green" }
+> = {
+  OPEN: { label: "Open", color: "red" },
+  IN_PROGRESS: { label: "In Progress", color: "violet" },
+  CLOSED: { label: "Done", color: "green" },
+};
+
+const IssueStatusBadge = ({ status }: Props) => {
+  const statusInfo = statusMap[status];
+  return <Badge color={statusInfo.color}>{statusInfo.label}</Badge>;
+};
+
+export default IssueStatusBadge;
