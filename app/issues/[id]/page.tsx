2755bb065d515e7d34b93f689772e696dc338741
Christine Yoo <christine.y@arketa.co>
2024-02-19 22:33:30 -0800

    Refactor: Organize import statements

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,8 @@
-import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { IssueStatusBadge } from "@/app/components";
+import prisma from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
-import React from "react";
 import ReactMarkdown from "react-markdown";
-import prisma from "@/prisma/client";
 
 interface Props {
   params: { id: string };
