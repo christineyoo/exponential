df5816c4edfa5d58d56d1d7aeb5ae0af368db6f8
Christine Yoo <christine.y@arketa.co>
2024-02-24 19:43:42 -0800

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -12,18 +12,35 @@ export async function PATCH(
     return NextResponse.json(validation.error.format(), { status: 400 });
   }
 
-  const issue = await prisma!.issue.findUnique({
+  const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
   if (!issue) {
-    return NextResponse.json("Issue not found", { status: 404 });
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
   }
 
-  const updatedIssue = await prisma!.issue.update({
+  const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: { title: body.title, description: body.description },
   });
 
   return NextResponse.json(updatedIssue);
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma!.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  await prisma.issue.delete({ where: { id: issue.id } });
+
+  return NextResponse.json({ message: "Issue deleted" });
 }
