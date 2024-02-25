51cd5ff6739e5a02ca66f0e306f660fcf7488657
Christine Yoo <christine.y@arketa.co>
2024-02-24 19:20:26 -0800

    Add a delete button

---
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -0,0 +1,14 @@
+import { TrashIcon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import React from "react";
+
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button color="red">
+      <TrashIcon />
+      Delete Issue
+    </Button>
+  );
+};
+
+export default DeleteIssueButton;
